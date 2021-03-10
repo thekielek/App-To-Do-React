459c42747f1f987fd17878796d4a1e67543d3c16
Lukasz Kielkowski <lukaszkielkowski@gmail.com>
2021-03-10 20:13:43 +0100

    add newDate variable, button delate

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,9 +2,15 @@ import React from "react";
 import "./Task.css";
 
 export const Task = ({ todo }) => {
+  const newDate = new Date().toLocaleTimeString();
+  console.log(newDate);
   return (
     <div className="task">
-      <p className="task__item">{todo.text}</p>
+      <p className="task__title">
+        {newDate}
+        {todo.text}
+      </p>
+      <button className="task__button">Usuń</button>
     </div>
   );
 };
